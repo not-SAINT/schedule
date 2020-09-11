65f19d157a39094561b14be1193c7282d7b71a77
Zhuchkou Artsiom <zidane.from.france@gmail.com>
2020-09-11 22:00:45 +0300

    feat/S-6.1 add deadline virtual event

--- schedule/src/App.tsx
+++ schedule/src/App.tsx
@@ -3,14 +3,16 @@ import { Provider } from 'mobx-react';
 import { ErrorBoundary } from 'react-error-boundary';
 
 import Settings from './mobx/store/settings';
+import Events from './mobx/store/events';
 import ErrorFallback from './components/ErrorFallback';
 import './App.css';
 
 const settings = new Settings();
+const events = new Events();
 
 const App = () => {
   return (
-    <Provider settings={settings}>
+    <Provider settings={settings} events={events}>
       <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => {}}>
         <div />
       </ErrorBoundary>
